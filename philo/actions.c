ebadc41a9e9a9b98c2e80a7d4c6f038a9b1f7c9f
Sahin Baran Essiz <sessiz@k1m09s03.42istanbul.com.tr>
2024-05-09 20:44:04 +0300

    test

--- philo/actions.c
+++ philo/actions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   actions.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sessiz <sessiz@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:51:28 by mitasci           #+#    #+#             */
-/*   Updated: 2024/05/09 19:16:56 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/05/09 20:43:30 by sessiz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,20 @@ void	ft_take_fork(t_philo *philo, t_table *table)
 	printf("fork2 id: %d\n", fork2->id);
 }
 
-void	ft_leave_fork(t_philo *philo, t_table table)
+void	ft_leave_fork(t_philo *philo, t_table *table)
 {
 	t_fork	*fork1;
 	t_fork	*fork2;
 
-	fork1 = &(table.forks[philo->id - 1]);
+	fork1 = &(table->forks[philo->id - 1]);
 	if (philo->id == 1)
-		fork2 = &(table.forks[table.philo_no - 1]);
+		fork2 = &(table->forks[table->philo_no - 1]);
 	else
-		fork2 = &(table.forks[philo->id - 2]);
+		fork2 = &(table->forks[philo->id - 2]);
 
 	pthread_mutex_unlock(&fork1->lock);
 	pthread_mutex_unlock(&fork2->lock);
+	ft_sleep(philo, *table);
 }
 
 void	ft_eat(t_philo *philo)
@@ -50,7 +51,7 @@ void	ft_eat(t_philo *philo)
 	ft_take_fork(philo, philo->table);
 	//write_message(ft_itoa(get_time() - table.start_time), ft_itoa(philo->id), "is eating\n");
 	ft_usleep(philo->table->time_to_eat);
-	ft_leave_fork(philo, *(philo->table));
+	ft_leave_fork(philo, philo->table);
 }
 
 void	ft_think(t_philo *philo, t_table table)
