6af6ea2edd8367b58aad540a63b604c75c720010
misratasci <misratasci@gmail.com>
2024-05-21 15:38:51 +0300

    bi kırtasiyeye gidip gelcem

--- philo/philo.c
+++ philo/philo.c
@@ -6,16 +6,29 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 16:33:09 by sessiz            #+#    #+#             */
-/*   Updated: 2024/05/21 15:30:17 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/05/21 15:35:36 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <errno.h>
 
+int ft_death_check(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->table->check_dead);
+	if(ft_get_time_of_ms() - philo->last_meal > philo->table->time_to_die)
+	{
+		printf("%llu %d died\n", ft_get_time_of_ms() - philo->table->start_time, philo->id);	
+		return (1);
+	}
+	pthread_mutex_unlock(&philo->table->check_dead);
+	return (0);
+}
+
 void ft_take_forks(t_philo *philo)
 {
-	
+	if (ft_death_check(philo))
+		return ;
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_lock(philo->lfork);
@@ -41,6 +54,8 @@ void ft_leave_forks(t_philo *philo)
 void ft_eat(t_philo *philo)
 {
 	ft_take_forks(philo);
+	if (ft_death_check(philo))
+		return ;
 	philo->last_meal = ft_get_time_of_ms();
 	printf("%llu %d is eating\n", philo->last_meal - philo->table->start_time, philo->id);
 	ft_msleep(philo->table->time_to_eat);
@@ -49,20 +64,10 @@ void ft_eat(t_philo *philo)
 
 void	ft_sleep(t_philo *philo)
 {
+	if (ft_death_check(philo))
+		return ;
 	printf("%llu %d is sleeping\n", ft_get_time_of_ms() - philo->table->start_time, philo->id);
 	ft_msleep(philo->table->time_to_sleep);
-}
-
-int ft_death_check(t_philo *philo)
-{
-	pthread_mutex_lock(&philo->table->check_dead);
-	if(ft_get_time_of_ms() - philo->last_meal > philo->table->time_to_die)
-	{
-		printf("%llu %d died\n", ft_get_time_of_ms() - philo->table->start_time, philo->id);	
-		return (1);
-	}
-	pthread_mutex_unlock(&philo->table->check_dead);
-	return (0);
 }
 
 void *ft_live(void *args)
@@ -73,7 +78,10 @@ void *ft_live(void *args)
 	while (1)
 	{	
 		if (ft_death_check(philo))
+		{
+			pthread_mutex_unlock(&philo->table->check_dead);
 			break;
+		}
 		ft_eat(philo);
 		ft_sleep(philo);
 	}
